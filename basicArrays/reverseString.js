d14787469128f2562023772d56e3aaf17f7d006b
KISHORE-SKY <skkishore1273@gmail.com>
2026-04-23 21:15:42 -0700

    revserse string with O(1)

---
+++ basicArrays/reverseString.js
@@ -0,0 +1,19 @@
+// space complexity O(1)
+
+function ReverseString(s) {
+    let lengthOfS = s.length;
+    let halfOfS = lengthOfS/2;
+
+    for(let i = 0; i < halfOfS; i++){
+       // swaping(i,n-1-i) i=2 ,lengthOfS-1-2 
+        let temporary=s[i];
+        s[i] = s[lengthOfS-1-i];
+        s[lengthOfS-1-i]=temporary;
+       
+    }
+    
+
+    return s;
+}
+
+console.log(ReverseString(['s','k','i','s','h','o','r','e']));
