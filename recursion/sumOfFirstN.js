74237a9ef068d94d857355e8eb539cdea29dc938
KISHORE-SKY <skkishore1273@gmail.com>
2026-05-05 20:55:46 -0700

    sum of N number problem using recursion

--- recursion/sumOfFirstN.js
+++ recursion/sumOfFirstN.js
@@ -6,5 +6,14 @@ function sumOFfirstNnumber(n){
     return n + sumOFfirstNnumber(n - 1);
 }
 
-console.log(sumOFfirstNnumber(10));
+console.log(sumOFfirstNnumber(5));
 
+
+
+// sum(5)
+// 5 + sum(5 - 1) => sum(4) = 15
+// 4 + sum(4 - 1) => sum(3) = 10
+// 3 + sum(3 - 1) => sum(2) = 6
+// 2 + sum(2 - 1) => sum(1) = 3
+// 1 + sum(1 - 1) => sum(0) = 1
+// 0
